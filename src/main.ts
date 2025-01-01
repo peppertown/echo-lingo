73190ff39ff51abfbca06b26bd5d5d845a38f0f9
Dong Hyun <decidere@naver.com>
2025-01-01 23:16:07 +0900

    [Feat]: 파이프 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 값 제거
+      forbidNonWhitelisted: true, // 정의되지 않은 값에 대한 에러 반환
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
