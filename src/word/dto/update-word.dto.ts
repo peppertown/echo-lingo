a9bf73031d562880b4a701f93bf40ebe52e0e2ed
Dong Hyun <decidere@naver.com>
2025-01-06 05:08:02 +0900

    [Feat]: 단어 수정 DTO 구현

--- src/word/dto/update-word.dto.ts
+++ src/word/dto/update-word.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateWordDto } from './create-word.dto';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { WordDto } from './word.dto';
 
-export class UpdateWordDto extends PartialType(CreateWordDto) {}
+export class UpdateWordDto extends PartialType(WordDto) {
+  @IsOptional()
+  @IsNotEmpty({ message: '수정할 내용을 입력해주세요.' })
+  word?: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: '수정할 내용을 입력해주세요.' })
+  mean?: string;
+}
