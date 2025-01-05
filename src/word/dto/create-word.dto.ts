73a4b08f6275eee6a3bd72e3979da1aa30b29eaf
Dong Hyun <decidere@naver.com>
2025-01-05 22:26:44 +0900

    [Feat]: 단어 등록 DTO 구현 (배열)

--- src/word/dto/create-word.dto.ts
+++ src/word/dto/create-word.dto.ts
@@ -1,1 +1,10 @@
-export class CreateWordDto {}
+import { WordDto } from './word.dto';
+import { IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateWordDto {
+  @IsArray() // 배열이어야 함을 명시
+  @ValidateNested({ each: true }) // 배열의 각 요소를 유효성 검사
+  @Type(() => WordDto) // 각 요소를 WordDto로 변환
+  words: WordDto[];
+}
