49813712ef89cdee4b2ac6d6498a857337708a34
Dong Hyun <decidere@naver.com>
2025-01-06 05:11:20 +0900

    [Comment]: 단어 API 주석 추가

--- src/word/word.controller.ts
+++ src/word/word.controller.ts
@@ -15,26 +15,31 @@ import { UpdateWordDto } from './dto/update-word.dto';
 export class WordController {
   constructor(private readonly wordService: WordService) {}
 
+  // 단어 전체 조회
   @Get()
   async getAllWords() {
     return await this.wordService.getAllWords();
   }
 
+  // 단어 추가
   @Post()
   async create(@Body() createWordDto: CreateWordDto) {
     return await this.wordService.create(createWordDto);
   }
 
+  // 단어 날짜별 조회
   @Get(':date')
   getWordsByDate(@Param('date') date: string) {
     return this.wordService.getWordsByDate(date);
   }
 
+  // 단어 수정
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateWordDto: UpdateWordDto) {
     return this.wordService.update(id, updateWordDto);
   }
 
+  // 단어 삭제
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.wordService.remove(id);
--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -6,6 +6,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class WordService {
   constructor(private readonly prisma: PrismaService) {}
+
+  // 단어 전체 조회
   async getAllWords() {
     return await this.prisma.word.findMany({
       orderBy: {
@@ -14,6 +16,7 @@ export class WordService {
     });
   }
 
+  // 단어 추가
   async create(createWordDto: CreateWordDto) {
     await this.prisma.word.createMany({
       data: createWordDto.words,
@@ -21,6 +24,7 @@ export class WordService {
     return { message: '등록이 완료되었습니다' };
   }
 
+  // 단어 날짜별 조회
   async getWordsByDate(date: string) {
     // date를 UTC 기준의 시작 날짜로 설정
     const startDate = new Date(`${date}T00:00:00.000Z`);
@@ -39,6 +43,7 @@ export class WordService {
     return words;
   }
 
+  // 단어 수정
   async update(id: number, updateWordDto: UpdateWordDto) {
     await this.prisma.word.update({
       where: { id },
@@ -48,6 +53,7 @@ export class WordService {
     return { message: '수정이 완료됐습니다.' };
   }
 
+  // 단어 삭제
   async remove(id: number) {
     await this.prisma.word.delete({
       where: { id },
