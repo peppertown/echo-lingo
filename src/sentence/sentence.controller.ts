ad29c574e63d8770c045650a252ffc78e3e93dbb
Dong Hyun <decidere@naver.com>
2025-01-12 02:30:57 +0900

    [Feat] 예문 생성 로직 구현

--- src/sentence/sentence.controller.ts
+++ src/sentence/sentence.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SentenceService } from './sentence.service';
 
 @Controller('sentence')
 export class SentenceController {
   constructor(private readonly sentenceService: SentenceService) {}
+
+  // 예문 생성
+  @Post()
+  createSentence(@Body() words) {
+    return this.sentenceService.createSentence(words);
+  }
 }
--- src/sentence/sentence.service.ts
+++ src/sentence/sentence.service.ts
@@ -8,4 +8,21 @@ export class SentenceService {
     private readonly prisma: PrismaService,
     private readonly openai: OpenAiService,
   ) {}
+  async createSentence(words) {
+    // 예문 생성
+    const result = await this.openai.generateExampleSentences(words);
+
+    // 예문 생성 후 파싱 및 id 추가
+    const sentences = [];
+    for (let i = 0; i < result.length; i++) {
+      const ex = JSON.parse(result[i].result)[0];
+      ex.word_id = words[i].id;
+      sentences.push(ex);
+    }
+
+    await this.prisma.sentence.createMany({
+      data: sentences,
+    });
+    return { success: true, message: '예문 생성이 완료되었습니다' };
+  }
 }
