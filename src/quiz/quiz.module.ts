dc741b722d4ac9944870d6c9894db11048dc2768
Dong Hyun <decidere@naver.com>
2025-02-15 22:21:37 +0900

    [Feat] 퀴즈 모듈 프리즈마 임포트

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuizController],
   providers: [QuizService],
 })
