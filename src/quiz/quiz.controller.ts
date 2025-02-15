351a0eb3b1c96f3c27973f968671c3ff5966fdc7
Dong Hyun <decidere@naver.com>
2025-02-15 22:36:52 +0900

    [Feat] 당일 복습 퀴즈 로직 구현

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class QuizController {
   constructor(private readonly quizService: QuizService) {}
+
+  // 당일 복습 퀴즈
+  @Get('review')
+  async reviewQuiz() {
+    return this.quizService.reveiwQuiz();
+  }
 }
--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -8,4 +8,21 @@ export class QuizService {
     private readonly prisma: PrismaService,
     private readonly dayjs: DayjsService,
   ) {}
+
+  // 당일 복습 퀴즈
+  async reveiwQuiz() {
+    const now = await this.dayjs.now();
+    const startDate = await this.dayjs.startDate(now);
+    const endDate = await this.dayjs.endDate(now);
+
+    return this.prisma.word.findMany({
+      where: {
+        created_at: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      include: { Sentence: true },
+    });
+  }
 }
