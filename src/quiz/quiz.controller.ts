ec06e541037144b1d2700c43be1287b834096538
Dong Hyun <decidere@naver.com>
2025-02-15 23:02:12 +0900

    [Feat] 주기 복습 퀴즈 로직 구현

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -10,4 +10,9 @@ export class QuizController {
   async todayReview() {
     return this.quizService.getQuiz('today');
   }
+
+  @Get('interval')
+  async intervalReview() {
+    return this.quizService.getQuiz('interval');
+  }
 }
