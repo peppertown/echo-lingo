bde6378bb8180062075d993172c6d08da9d8cdcc
Dong Hyun <decidere@naver.com>
2025-03-09 19:01:46 +0900

    [Feat] 단어 레벨 통계 조회 로직 구현

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
+
+  // 단어 레벨 통계
+  @Get('word')
+  async getWordsLevelStats() {
+    return await this.dashboardService.getWordsLevelStats();
+  }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 단어 레벨 통계
+  async getWordsLevelStats() {
+    const result = await this.prisma.word.groupBy({
+      by: ['level'],
+      _count: {
+        id: true,
+      },
+    });
+
+    return result.map((res) => ({
+      level: res.level,
+      count: res._count.id,
+    }));
+  }
+}
